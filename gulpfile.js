0fcdf758d1ec5bbbe91be721e168a20101d1819a
Yana Taran <nachberlin@gmail.com>
2017-08-09 23:45:40 +0200

    working slick gallery

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var sourcemaps = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var run = require("run-sequence");
+var del = require("del");
+
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 2 versions"
+      ]}),
+      mqpacker({
+        sort:true
+      })
+    ]))
+    .pipe(sourcemaps.write())
+//    .pipe(gulp.dest("build/css"))
+//    .pipe(minify())
+//    .pipe(rename("style.min.css"))
+//    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("html:copy", function() {
+  return gulp.src("*.html")
+ //   .pipe(gulp.dest("build"));
+});
+
+gulp.task("html:update", ["html:copy"], function(done) {
+  server.reload();
+  done();
+});
+
+
+gulp.task("serve", function() {
+  server.init({
+//    server: "build/",
+    server: true,
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html", ["html:update"]);
+});
+
+
+gulp.task("copy", function() {
+  return gulp.src([
+      "fonts/**/*.{woff,woff2}",
+      "img/**",
+      "js/**",
+      "*.html"
+    ], {
+      base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    fn
+  );
+});
